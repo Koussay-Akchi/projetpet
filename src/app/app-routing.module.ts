8c2c0bf6a3b786508ded5643d48915495725a6ae
Koussay Akchi <kaskousakchi@gmail.com>
2023-04-07 22:48:33 +0100

    interfaces inscriptions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
 import { MagasinComponent } from './magasin/magasin.component';
-import { LoginUserComponent } from './login-user/login-user.component';
-import { LoginVetoComponent } from './login-veto/login-veto.component';
 import { InscriptionUserComponent } from './inscription-user/inscription-user.component';
 import { InscriptionVetoComponent } from './inscription-veto/inscription-veto.component';
 const routes: Routes = [
