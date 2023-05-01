a82109707c7af9414ee7c0f1969e43784f6d1819
Koussay Akchi <kaskousakchi@gmail.com>
2023-05-01 18:38:47 +0100

    oussema

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,10 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
   id_animal=645;
+  id_veto=2;
 
 
   set_id_animal(id: number) {
     this.id_animal = id;
+  }
+
+  set_id_veto(id: number) {
+    this.id_veto = id;
   }
 
   constructor() { }
